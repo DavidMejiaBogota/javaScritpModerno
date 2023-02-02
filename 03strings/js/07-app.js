73f5e24e2d51a8d75d5284d70239bc2ac18701a5
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-02 14:02:33 -0500

    22 String Methods - covert to uppercase or lowercase

---
+++ 03strings/js/07-app.js
@@ -0,0 +1,14 @@
+const producto10 =  "Monitor de 18 Pulgadas";
+//Para pasar strings a mayúsculas se usa la función toUpperCase
+console.log(producto10.toUpperCase());
+
+//Para pasar strings a minúsculas se usa la función toLowerCase
+console.log(producto10.toLowerCase());
+
+const email = "CORREO@CORREO.COM";
+console.log(email.toLocaleLowerCase());
+
+//toString convierte de # a string
+const precios = 300;
+console.log(precios);
+console.log(precios.toString());
