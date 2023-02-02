f8de50c05c71acfd2d6b10f67e5a1e1ddaa0b9d5
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-02 15:58:43 -0500

    The Object Math

---
+++ 04numeros/js/03-app.js
@@ -0,0 +1,53 @@
+console.log("Tema 3, funciones de Math");
+
+//PI
+resultado = Math.PI;
+
+//Redondear hacia o abajo dependiendo de los deceimales
+resultado = Math.round(2.6);
+resultado = Math.round(2.1);
+resultado = Math.round(2.6);
+resultado = Math.round(2.5);
+resultado = Math.round(2.4);
+
+//Redondear hacia arriba
+resultado = Math.ceil(2.1);
+resultado = Math.ceil(2.2);
+resultado = Math.ceil(2.3);
+resultado = Math.ceil(2.4);
+resultado = Math.ceil(2.5);
+resultado = Math.ceil(2.6);
+resultado = Math.ceil(2.7);
+resultado = Math.ceil(2.8);
+resultado = Math.ceil(2.9);
+
+//redondear hacía abajo
+resultado = Math.floor(2.9);
+
+//raiz cuadrada
+resultado = Math.sqrt(144)
+
+//Valor absoluto
+resultado = Math.abs(-500);
+
+//Potencia
+
+resultado = Math.pow(8, 3);
+resultado = Math.pow(2, 4);
+
+//Minímo 
+resultado = Math.min(3, 5);
+
+//Maxímo
+resultado = Math.max(3, 5, 12, 18)
+
+
+//Aleatorio
+resultado = Math.random();
+//se puede multiplicar 
+resultado = Math.random() * 20;
+ 
+//Aleatorio dentro de un rango
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
