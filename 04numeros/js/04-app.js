b6a3e701c141ea64e667f7407d5c7f7a017c6ef7
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 10:45:16 -0500

    Operations Order

---
+++ 04numeros/js/04-app.js
@@ -0,0 +1,17 @@
+console.log("Theme 4, Exercise 4");
+
+let resultados;
+
+//Las operaciones e realizan primero las multipilicaciones y divisiones y luego si sumas y restas
+resultados = (20+30) * 2
+resultados = 20+30 * 2
+
+//20% descuento en compras
+resultados = (20+30+40+50+60) *.2
+
+//16% de impuesto
+resultados = (20+30) * 1.16
+
+//Es muu útil controlar el orden en el que se van a realizar las operaciones
+
+console.log(resultados);
