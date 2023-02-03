842956b9b3c2836ced21a1e6afa017da0e09b954
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 15:59:33 -0500

    32 Create and Compare Booleans

---
+++ 06booleans/js/01-app.js
@@ -0,0 +1,16 @@
+console.log("Theme 5: Boolenas - Exercise 01: Create and Compare Booleans")
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+
+console.log(boolean1);
+console.log(boolean2);
+
+console.log(typeof boolean1, typeof boolean2);
+
+console.log(boolean1 == boolean3);
+console.log(boolean1 === boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(typeof boolean4);
