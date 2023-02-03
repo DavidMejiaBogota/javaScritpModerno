0874400334679a79e8f4bf0492cfa6cf28b12059
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 11:33:32 -0500

    27 Convert Strings to Numbers

---
+++ 04numeros/js/06-app.js
@@ -0,0 +1,18 @@
+console.log("Theme 4 Numbers, Exercise 6 Convert strings to numbers");
+
+const number0 = "20";
+const number1 = "20.2";
+const number2 = "uno";
+const number3 = 20;
+
+console.log(number0);
+console.log(Number.parseInt(number0));
+console.log(Number.parseFloat(number1));
+console.log(Number.parseInt(number2));
+
+//resvisar si el número es enstero o no
+
+console.log(Number.isInteger(number0));
+console.log(Number.isInteger(number1));
+console.log(Number.isInteger(number2));
+console.log(Number.isInteger(number3));
