b019e2b1a3c69e211589b8c0a941a33a3b70249e
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 15:04:13 -0500

    30 Compare Values

---
+++ 05operators/js/02-app.js
@@ -0,0 +1,25 @@
+console.log("Theme 5 - Numbers Operator - Exercise 5 - Compare Values")
+
+const number4 = 20;
+const number5 = "20";
+const number6 = 30;
+
+//revisar si 2 números son iguales
+
+console.log("number4 = "+ number4)
+console.log("number5 = "+ number5)
+console.log("number6 = "+ number6)
+
+//== si es igual a otr@ elemento
+console.log("number4 == number5",number4 == number5);
+//== si es exsactamente igual a otr@ elemento, incluido el tipo de dato
+console.log("number4 === number5",number4 === number5);
+
+//!= diferente de otr@ elemento
+const password0 = "admin";
+const password1 = "Admin";
+
+console.log("password0 != password1: ",password0 != password1);
+
+console.log("number4 != number5: ",number4 != number5);
+console.log("number4 != number5: ",number4 != number6);
