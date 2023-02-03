e3a906630973a81f2357087bcdec023bff345a1b
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 15:29:25 -0500

    31 Compare Null and Undefined

---
+++ 05operators/js/03-app.js
@@ -0,0 +1,19 @@
+console.log("Theme 5 Operators - Exercise 3 - Compare null and undefined");
+
+
+let number7;
+console.log(number7);
+console.log(typeof number7);
+
+//null
+let number8 = null;
+console.log(number8);
+console.log(typeof number8);
+
+//comparador sencillo ==
+console.log(number7 == number8);
+//comparador estricto ===
+console.log(number7 === number8);
+
+
+
