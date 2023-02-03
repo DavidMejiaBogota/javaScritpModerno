47e5b0a95b8ac0d42a8bd6a0a0c14a2ad59602b4
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 11:11:19 -0500

    26 Increase and Decrease

---
+++ 04numeros/js/05-app.js
@@ -0,0 +1,28 @@
+console.log("Theme 4, Exsrcise 5");
+
+let puntaje1 = 5;
+let puntaje2 = 5;
+
+puntaje1++;
+++puntaje1; 
+
+puntaje2--//5
+--puntaje2//3
+
+
+//el puntaje puede ir aumentando en valores más grande que uno en uno, se especifica
+
+puntaje1 +=3;
+puntaje1 +=3;
+puntaje1 +=3;
+puntaje1 +=3;
+
+//así mismo puede ir hacía abjo.
+
+puntaje2 -=10 
+puntaje2 -=10 
+puntaje2 -=10 
+puntaje2 -=10 
+
+console.log("Punaje1 = "+puntaje1);
+console.log("Punaje2 = "+puntaje2);
