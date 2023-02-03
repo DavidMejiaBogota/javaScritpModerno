277e023a13f36afa8ff638eb0e0f6c03ffcb2f1f
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-03 12:07:33 -0500

    28 Comparison Operators - Less Than and Greater Than

---
+++ 05operators/js/01-app.js
@@ -0,0 +1,13 @@
+console.log("Theme 5 Numbers Operators, Exercise 1 - Less Than and Greater Than");
+
+const number1 = 20;
+const number2 = "20";
+const number3 = 30;
+
+//operator less than >
+console.log(number1 > number3);
+console.log(number3 > number1);
+
+//operator greaster than <
+console.log(number1 < number3);
+console.log(number3 < number1);
