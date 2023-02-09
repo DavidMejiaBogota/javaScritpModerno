df783fb4736b02edb7cb83fada4ae1490f83107f
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-09 10:19:44 -0500

    35 Create Objects In JavaScript

---
+++ 07objetos/js/01-app.js
@@ -0,0 +1,17 @@
+console.log("Theme 07 Objects - Exercise 1 - Create Ojbects in JavaScript");
+
+//se puede crear variables/constates para un objeto
+
+const nombre = "Monitor de 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+//Ó se pede crear un objeto con sus variables/constantes
+
+const producto = {
+    nombre : "Monitor de 20 Pulgadas",
+    precio : 300,
+    disponible : true,
+};
+
+console.log(producto);
