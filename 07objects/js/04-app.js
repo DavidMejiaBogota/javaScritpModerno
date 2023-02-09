1cbd1ff55c7e1b854f0830a0aa4025c098b65ddf
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2023-02-09 11:57:08 -0500

    38 Desctructuring Objects

---
+++ 07objects/js/04-app.js
@@ -0,0 +1,11 @@
+console.log("Theme 07 Objects - Exercise 04 - Destructuring of Objects");
+
+const producto4 = {
+    nombre4 : "Hidrolavadora",
+    precio4 : 25,
+    disponible4 : true,
+};
+
+//Destructuring
+const { nombre4, precio4, disponible4 } = producto4;
+console.log(nombre4);
